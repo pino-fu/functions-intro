2ac35ac835d3f8935e7892074d5994a0341bcd38
Ben Hoberg <benjhoberg@gmail.com>
2022-12-12 10:22:10 -0600

    JS challenge, just an intro to functions.

---
+++ main.js
@@ -0,0 +1,8 @@
+const fillGasTank = (gallons) => {
+    if (gallons > 15) {
+        console.log("You cannoot add this much gas to the tank")
+    } else {
+    console.log(`I filled the tank with ${gallons} of gas`)
+} 
+}
+fillGasTank(16)
